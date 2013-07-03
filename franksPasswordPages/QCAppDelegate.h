0b2785900b368f882afe5626d12920fb242ee4b8
frankleo <thisisfrankrivera@gmail.com>
2013-07-03 11:08:46 -0400

    Initial Commit

---
+++ franksPasswordPages/QCAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  QCAppDelegate.h
+//  franksPasswordPages
+//
+//  Created by Frank Rivera on 7/3/13.
+//  Copyright (c) 2013 Frank Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class QCViewController;
+
+@interface QCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) QCViewController *viewController;
+
+@end
---
+++ franksPasswordPages/QCViewController.h
@@ -0,0 +1,13 @@
+//
+//  QCViewController.h
+//  franksPasswordPages
+//
+//  Created by Frank Rivera on 7/3/13.
+//  Copyright (c) 2013 Frank Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QCViewController : UIViewController
+
+@end
